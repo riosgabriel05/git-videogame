c3b5153ce1cb6db721e9f54949929fa3449a3d84
unknown <riosgabriel05@gmail.com>
2026-09-06 19:24:46 -0300

    cambio 3

--- src/domain/GamesList.ts
+++ src/domain/GamesList.ts
@@ -20,7 +20,10 @@ export class GamesList {
   // Si genre es 'All', retornar todos los juegos.
   // Usá el método .filter(x => condición) del array this.games para filtrar por genre.
   filterByGenre(_genre: string): Game[] {
-    return this.games
+    if (_genre=="All"){
+      return this.games;
+    }
+    return this.games.filter(game => game.genre === _genre);
   }
 
   count(): number {
